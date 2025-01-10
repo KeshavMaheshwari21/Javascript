a6177b70420479a9c85cb5c7d1d3bf4213a68950
KeshavMaheshwari21 <beastkeshavmaheshwari51@gmail.com>
2025-01-10 23:32:57 +0530

    Operations

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -26,3 +26,37 @@ let someNumber = 11
 let stringNumber = Number(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ******************* Operations *******************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2 + 2); -> Addition
+// console.log(2 - 2); -> Subtraction
+// console.log(2 * 2); -> Multiplication
+// console.log(2 / 2); -> Division
+// console.log(2 ** 3); -> Power
+// console.log(2 % 2); -> Modulus
+
+let str1 = "hello"
+let str2 = " abc"
+let str3 = " def"
+
+let str4 = str1 + str2 + str3 // addition of the 2 (or more) string is possible not the subtraction
+console.log(str4);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(false); // false
+console.log(+false); // 0
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
