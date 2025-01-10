f727b27fb843a6668e18991437e6052e4e179086
KeshavMaheshwari21 <beastkeshavmaheshwari51@gmail.com>
2025-01-10 21:51:10 +0530

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert("hello") --> using nodejs instead of the browser
+
+let name = "javascript"; // string
+let number = 5; // number
+let isLoggedIn = true // boolean
+
+// number -> 2 to 2^53
+// bigint
+// string -> " "
+// boolean -> true/false
+// null -> standalone value (representation of empty value)
+// undefined -> the value is not assigned
+// symbol -> used for the uniqueness
+
+// object
+
+console.log(typeof name);
